8c02266afad5ef459577423e8057478a4d187f8b
kaustubh <kaustubw@amazon.com>
2025-03-02 23:18:07 -0800

    feat: added caps to caps if pressed with shift

--- src/caps_lock_mods.ts
+++ src/caps_lock_mods.ts
@@ -2,12 +2,18 @@ import { rule, map } from "karabiner.ts"
 
 export const rules = [
     rule('Caps Lock → Hyper/Escape')
-    .description('Caps Lock is escape if pressed alone or hyper when pressed with modifier.')
-    .manipulators([
-        map('caps_lock')
-        .toHyper()
-        .toIfAlone('escape')
-    ]),
+        .description('Caps Lock is escape if pressed alone or hyper when pressed with modifier.')
+        .manipulators([
+            map('caps_lock')
+            .toHyper()
+            .toIfAlone('escape')
+        ]),
+    rule('Shift + Caps Lock → Caps Lock')
+        .description('Caps Lock is capslock if pressed with shift.')
+        .manipulators([
+            map('caps_lock', 'shift')
+            .to('caps_lock')
+        ])
 ]
 
 
