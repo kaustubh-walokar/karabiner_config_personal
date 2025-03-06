1d71a1f4aaed0c3b442f2fc5e9108b3516d77706
kaustubh <kaustubw@amazon.com>
2025-03-06 09:05:26 -0800

    feat: updated the open app manipulator

--- src/caps_lock_mods.ts
+++ src/caps_lock_mods.ts
@@ -1,21 +1,21 @@
-import { rule, map, simlayer } from "karabiner.ts"
+import { rule, map, simlayer, withModifier } from "karabiner.ts"
 
 export const rules = [
     rule('Caps Lock → Hyper/Escape')
-        .description('Caps Lock is escape if pressed alone or hyper when pressed with modifier.')
-        .manipulators([
-            map('caps_lock')
-                .toHyper()
-                .toIfAlone('escape'),
-        ]),
+    .description('Caps Lock is escape if pressed alone or hyper when pressed with modifier.')
+    .manipulators([
+        map('caps_lock')
+        .toHyper()
+        .toIfAlone('escape'),
+    ]),
     simlayer('o', 'Open Apps with Hyper')
-        .description('Open Apps with Hyper')
-        .manipulators([
-            map('b', 'Hyper').to$(`open -a "Firefox"`),
-            map('s', 'Hyper').to$(`open -a "Slack"`),
-            map('t', 'Hyper').to$(`open -a "Ghostty"`),
-            map('i', 'Hyper').to$(`open -a "IntelliJ IDEA Ultimate"`),
-        ]),
+    .description('Open Apps with Hyper')
+    .manipulators([withModifier('Hyper')([
+        map('b').to$(`open -a "Firefox"`),
+        map('s').to$(`open -a "Slack"`),
+        map('t').to$(`open -a "Ghostty"`),
+        map('i').to$(`open -a "IntelliJ IDEA Ultimate"`),
+    ])]),
 ]
 
 
