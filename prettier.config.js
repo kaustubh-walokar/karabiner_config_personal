fd7957086fecbf4f9c0b9fc0c782c7f0154280e5
kaustubh <kaustubw@amazon.com>
2025-03-06 09:14:22 -0800

    feat: updated config an build script

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,10 @@
+const { table } = require('console')
+const prettier = require('prettier')
+
 /** @type {import('prettier').Options} */
 module.exports = {
+  tabWidth: 2,
+  useTabs: false,
   singleQuote: true,
   semi: false,
   trailingComma: 'all',
